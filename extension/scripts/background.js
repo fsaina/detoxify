ea7481670d27607eea7ca910b252828529fcec35
vernierm <vernier.mt@gmail.com>
2019-04-06 20:16:30 +0200

    Fix url reload bug

---
+++ extension/scripts/background.js
@@ -0,0 +1,25 @@
+console.log('Background script started.');
+
+window.browser = (function () {
+    return window.msBrowser ||
+        window.browser ||
+        window.chrome;
+})();
+
+function isYoutubeUrl(url){
+    var regExp = /^.*(youtube.com\/).*/;
+    return url.match(regExp);
+}
+
+function notifyTabUrlUpdated(tabId) {
+    browser.tabs.sendMessage(tabId, {
+        message: 'tab-updated'
+    })
+}
+
+browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url !== undefined && isYoutubeUrl(changeInfo.url)) {
+        console.log('Updated tab #' + changeInfo.url);
+        notifyTabUrlUpdated(tabId);
+    }
+});
